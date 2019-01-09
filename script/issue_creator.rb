5441853dd337ae82c7626040f66973e74f9f9739
Yuki AKAMATSU <yuki-akamatsu@cookpad.com>
2019-01-09 20:32:31 +0900

    Create two issues about record/publish movie

--- script/issue_creator.rb
+++ script/issue_creator.rb
@@ -27,3 +27,19 @@ milestone_number = client.create_milestone(repo, milestone_title)[:number]
 issue_titles.each do |issue_title|
   client.create_issue(repo, issue_title, nil, { milestone: milestone_number })
 end
+
+publish_movie_issue_description = <<~PMD
+ - [ ] crash academyさんに編集完了がいつ頃か確認をする
+ - [ ] 発表者で動画の確認が必要な人に動画の確認をしてもらう
+   - [ ] 動画確認が必要な人の名前を書く
+ - [ ] crash academyさんに動画の公開の連絡をする
+   - [ ] 公開確認をしてIssueをクローズ
+PMD
+client.create_issue(repo, "##{number - 1} の動画を公開する", publish_movie_issue_description, { milestone: milestone_number })
+
+request_movie_issue_description = <<~RMD
+ - [ ] 会場がクックパッド/クラウドワークス以外なら、撮影可否と設備について確認をする
+ - [ ] crash academyさんに動画撮影の依頼をする
+   - [ ] 返事を確認してIssueをクローズ
+RMD
+client.create_issue(repo, "動画撮影の依頼をする", request_movie_issue_description, { milestone: milestone_number })
